d55f6a425cedae714836164eb8072d926841125f
dodiaz <dad358@cornell.edu>
2020-12-17 16:43:39 -0500

    Update TorchNN_MPI.py

--- TorchNN_MPI.py
+++ TorchNN_MPI.py
@@ -49,6 +49,98 @@ class Model(torch.nn.Module):
         return pred
     
     
-print("Hello")
     
     
+## Train the model
+    
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+
+mnist_valset, mnist_testset = torch.utils.data.random_split(mnist_testset, [int(0.9 * len(mnist_testset)), int(0.1 * len(mnist_testset))])
+
+train_dataloader = torch.utils.data.DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+val_dataloader = torch.utils.data.DataLoader(mnist_valset, batch_size=32, shuffle=False)
+test_dataloader = torch.utils.data.DataLoader(mnist_testset, batch_size=32, shuffle=False)
+
+print("Training dataset size: ", len(mnist_trainset))
+print("Validation dataset size: ", len(mnist_valset))
+print("Testing dataset size: ", len(mnist_testset))
+
+
+model = Model()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+no_epochs = 1
+train_loss = list()
+val_loss = list()
+best_val_loss = 1
+for epoch in range(no_epochs):
+    total_train_loss = 0
+    total_val_loss = 0
+
+    model.train()
+    # training
+    for itr, (image, label) in enumerate(train_dataloader):
+
+        optimizer.zero_grad()
+
+        pred = model(image)
+
+        loss = criterion(pred, label)
+        total_train_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+
+    total_train_loss = total_train_loss / (itr + 1)
+    train_loss.append(total_train_loss)
+
+    # validation
+    model.eval()
+    total = 0
+    for itr, (image, label) in enumerate(val_dataloader):
+
+        pred = model(image)
+
+        loss = criterion(pred, label)
+        total_val_loss += loss.item()
+
+        pred = torch.nn.functional.softmax(pred, dim=1)
+        for i, p in enumerate(pred):
+            if label[i] == torch.max(p.data, 0)[1]:
+                total = total + 1
+
+    accuracy = total / len(mnist_valset)
+
+    total_val_loss = total_val_loss / (itr + 1)
+    val_loss.append(total_val_loss)
+
+    print('\nFrom processor ' + str(rank) + ' Epoch: {}/{}, Train Loss: {:.8f}, Val Loss: {:.8f}, Val Accuracy: {:.8f}'.format(epoch + 1, no_epochs, total_train_loss, total_val_loss, accuracy))
+
+    if total_val_loss < best_val_loss:
+        best_val_loss = total_val_loss
+        print("Saving the model state dictionary for Epoch: {} with Validation loss: {:.8f}".format(epoch + 1, total_val_loss))
+        torch.save(model.state_dict(), "model.dth")
+
+
+# test model
+model.load_state_dict(torch.load("model.dth"))
+model.eval()
+
+results = list()
+correct = 0
+for itr, (image, label) in enumerate(test_dataloader):
+
+    pred = model(image)
+    pred = torch.nn.functional.softmax(pred, dim=1)
+
+    for i, p in enumerate(pred):
+        if label[i] == torch.max(p.data, 0)[1]:
+            correct = correct + 1
+            results.append((image, torch.max(p.data, 0)[1]))
+
+test_accuracy = correct/len(mnist_testset)
+print('\nTest accuracy {:.8f}'.format(test_accuracy))
+
